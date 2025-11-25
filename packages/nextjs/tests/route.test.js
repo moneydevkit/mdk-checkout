ef15ce8c4815828dd6cf14456d2ab8f73ea028c2
Martin Saposnic <martinsaposnic@gmail.com>
2025-11-25 14:41:35 -0300

    fixup: tesT

--- packages/nextjs/tests/route.test.js
+++ packages/nextjs/tests/route.test.js
@@ -6,18 +6,13 @@ import { fileURLToPath } from "node:url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const distIndex = path.join(__dirname, "../dist/index.js");
-const distServer = path.join(__dirname, "../dist/server/index.js");
 const pkgJsonPath = path.join(__dirname, "../package.json");
 
 test("build emits top-level bundle", () => {
   assert.ok(existsSync(distIndex), "dist/index.js should exist");
 });
 
-test("build emits server bundle", () => {
-  assert.ok(existsSync(distServer), "dist/server/index.js should exist");
-});
-
 test("package exports contain server entry", () => {
   const pkgJson = JSON.parse(readFileSync(pkgJsonPath, "utf8"));
-  assert.ok(pkgJson.exports?.["./server"], "./server export must be defined");
+  assert.ok(pkgJson.exports?.["./server/route"], "./server/route export must be defined");
 });
