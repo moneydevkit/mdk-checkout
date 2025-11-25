e9b7848c35d2c60aa7e0ff882ebcfeab68dcd089
Martin Saposnic <martinsaposnic@gmail.com>
2025-11-25 14:41:28 -0300

    delete unused exports and runtime

--- packages/nextjs/src/server/runtime.ts
+++
@@ -1,9 +0,0 @@
-'use server'
-
-export {
-  createMoneyDevKitClient,
-  createMoneyDevKitNode,
-  resolveMoneyDevKitOptions,
-} from './mdk'
-export { MoneyDevKitClient, MoneyDevKitNode } from './money-dev-kit'
-export type { MoneyDevKitOptions } from './money-dev-kit'
